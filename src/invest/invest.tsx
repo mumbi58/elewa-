8d55d69a15b1c2650d82e55a6d07789d6d39d89f
regina <reggiekariuki58@gmail.com>
2023-11-02 15:12:48 +0300

    styled the hero section

--- src/invest/invest.tsx
+++ src/invest/invest.tsx
@@ -1,11 +1,100 @@
 import Navbar from "../About/navbar";
+import "./invest.css"
 const Invest = () => {
     return ( 
         <div className="invest">
+            <div className="hero">
 <Navbar />
 
 <h3>Trade, not aid</h3>
-<p>Southern innovations addressing global problems</p>
+<p>Southern innovations addressing <br></br>global problems</p>
+</div>
+
+<div className="nav2">
+    <h2>How we see it differently</h2>
+    <p>The global South is rapidly catching up with the "developed" world. It's time for you to get to know the rapid development, innovation and drive going on in this part of the world (East-Africa, South Asia and beyond).</p>
+    <div className="img"></div>
+</div>
+
+<div className="figures">
+    <h2>Key figures</h2>
+    <div className="figurecards">
+        <div className="cards">
+            <h2>1.3M</h2>
+            <p>Approx. number of Kenyan talents reaching the age of 18 in 2022</p>
+        </div>
+        <div className="cards">
+            <h2>5M</h2>
+            <p>number of East-Africans(EAC) reaching the age of 18 in 2022</p>
+        </div>
+        <div className="cards">
+            <h2>2022</h2>
+            <p>Democratic Republic of Congo joins the East African Community</p>
+        </div>
+        <div className="cards">
+            <h2>1.5M</h2>
+            <p>The number of unfilled IT vacancies in the US and EU markets</p>
+        </div>
+        <div className="cards">
+            <h2>1.3M</h2>
+            <p>Approx. number of Kenyan talents reaching the age of 18 in 2022</p>
+        </div>
+        <div className="cards">
+            <h2>10M</h2>
+            <p>Number of technical vacancies by 2030</p>
+        </div>
+        <div className="cards">
+            <h2>2050</h2>
+            <p>Africa holds the worlds largest workforce</p>
+        </div>
+        <div className="cards">
+            <h2>4.6M</h2>
+            <p>Startup funding raised by African startups in 2022</p>
+
+        </div>
+        <div className="cards">
+            <h2>310%</h2>
+            <p>Increase in startup funding for companies with HQ in Kenya</p>
+            
+        </div>
+    </div>
+</div>
+
+<div className="informartion">
+    <div className="img2"></div>
+    <div className="content">
+        <h2>Elewa NV, a multi-stakeholder cooperation</h2>
+        <p>At the heart of Elewa lies the structure Elewa NV. Based in Brussels, Belgium, Elewa NV is a private holding company which controls the assets of all Elewa activities.
+
+Elewa NV is owned and controlled by Elewa's founder, a small community of investors which share in the vision of Elewa and Elewa employees based throughout the activities of the group.</p>
+    </div>
+</div>
+
+<div className="information2">
+    <div className="content2">
+        <h2>The Elewa Creative Hub
+</h2>
+<p>Coming soon; The Elewa Creative Hub Lies at the heart of our organization. A two-acre property at the center of Nairobi's creative district, the hub connects all Elewa's Activities under a single banner.
+
+Through partnerships with creative community, the Elewa Hub will organize vibrant activites that bring talents from different fields (tech, business, art, fashion) together to nurture continuous innovat</p>
+    </div>
+    <div className="img3"></div>
+</div>
+
+<div className="information3">
+    <div className="img4"></div>
+    <div className="content3">
+        <h2>Investing in Elewa NV
+</h2>
+<p>From time to time, we offer investment opportunities (private equity & debt) to out community of investors. Interested to take part when this opportunity arrives?</p>
+<button>join the waiting list</button>
+    </div>
+</div>
+
+<div className="info">
+    <h2>Become part of our mission.
+And start investing</h2>
+</div>
 
         </div>
      );
